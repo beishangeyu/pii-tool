cc2c86afba334acc622dbe5214ae6ffca8b98b07
4090 <4090>
2025-10-01 16:49:40 +0800

    上传piianalyzer的脚本

---
+++ piianalyzer/scripts/run_c4.sh
@@ -0,0 +1,7 @@
+export CLASSPATH='./stanford-ner.jar'
+python run.py \
+    --dataset_name c4 \
+    --data_path '../data/c4/en' \
+    --batch_size 1000 \
+    --workers 75 \
+    --max_tasks_per_child 5
---
+++ piianalyzer/scripts/run_googlenq.sh
@@ -0,0 +1,7 @@
+export CLASSPATH='./stanford-ner.jar'
+python run.py \
+    --dataset_name googlenq \
+    --data_path '../data/googlenq' \
+    --batch_size 1000 \
+    --workers 16 \
+    --max_tasks_per_child 5
