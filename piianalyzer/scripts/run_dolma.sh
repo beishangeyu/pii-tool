195ac6c77fa35020beda9513eeed9b4f04e853ba
a6k <a6k>
2025-10-06 23:27:53 +0800

    update

--- piianalyzer/scripts/run_dolma.sh
+++ piianalyzer/scripts/run_dolma.sh
@@ -3,5 +3,5 @@ python run.py \
     --dataset_name dolma \
     --data_path '../data/dolma/v1_7' \
     --batch_size 60 \
-    --workers 42 \
-    --max_tasks_per_child 5
+    --workers 96 \
+    --max_tasks_per_child 10
