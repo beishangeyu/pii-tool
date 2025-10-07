fa530d540520125a9aefd0af35e4ba3720ecc76b
a6k <a6k>
2025-10-07 13:28:14 +0800

    update

--- piianalyzer/scripts/run_dolma.sh
+++ piianalyzer/scripts/run_dolma.sh
@@ -1,7 +1,7 @@
-export CLASSPATH='./stanford-ner.jar'
+export CLASSPATH='/mnt/mingd3/PII_detect/piianalyzer/stanford-ner.jar'
 python run.py \
     --dataset_name dolma \
     --data_path '../data/dolma/v1_7' \
     --batch_size 60 \
-    --workers 96 \
-    --max_tasks_per_child 10
+    --workers 72 \
+    --max_tasks_per_child 5
