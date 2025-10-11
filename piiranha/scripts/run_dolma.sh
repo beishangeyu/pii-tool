de4c93742e6f246a1f16c8300097141a6e3b3997
a6k <a6k>
2025-10-11 20:00:32 +0800

    update

--- piiranha/scripts/run_dolma.sh
+++ piiranha/scripts/run_dolma.sh
@@ -1,5 +1,5 @@
 python run.py \
     --dataset_name dolma \
     --data_path '../data/dolma/v1_7' \
-    --batch_size 2048 \
+    --batch_size 12 \
     --device 1
--- starpii/scripts/run_dolma.sh
+++ starpii/scripts/run_dolma.sh
@@ -1,5 +1,5 @@
 python run.py \
     --dataset_name dolma \
     --data_path '../data/dolma/v1_7' \
-    --batch_size 128 \
-    --device 3
+    --batch_size 12 \
+    --device k3
